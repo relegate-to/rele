4fec88a11dee767a91034fb76d0829120fa3824b
Sam <sam.finchett@pm.me>
2026-03-15 20:45:54 +0900

    fix: exclude / and /sign-in from auth middleware

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -4,7 +4,7 @@ export default auth.middleware({ loginUrl: '/sign-in' });
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    '/home/:path*',
+    '/api/:path*',
   ],
 };
